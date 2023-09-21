ecf9ee16ab754481b8446dc56971d1a622679453
faCompteGit <fafriday22@gmail.com>
2023-09-21 08:44:17 +0100

    Backend commit2

--- serviceSoap.py
+++ serviceSoap.py
@@ -1,58 +1,6 @@
 import zeep
 import json
-  
-# # set the WSDL URL
-# wsdl_url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL"
-  
-# # set method URL
-# method_url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryIntPhoneCode"
-# # set service URL
-# service_url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso"
-  
-# # create the header element
-# header = zeep.xsd.Element(
-#     "Header",
-#     zeep.xsd.ComplexType(
-#         [
-#             zeep.xsd.Element(
-#                 "{http://www.w3.org/2005/08/addressing}Action", zeep.xsd.String()
-#             ),
-#             zeep.xsd.Element(
-#                 "{http://www.w3.org/2005/08/addressing}To", zeep.xsd.String()
-#             ),
-#         ]
-#     ),
-# )
-# # set the header value from header element
-# header_value = header(Action=method_url, To=service_url)
-  
-# # initialize zeep client
-# client = zeep.Client(wsdl=wsdl_url)
 
-# # variable d'entrée du programme
-# pays_code ="GA"
-  
-
-# result = client.service.CountryFlag(
-#   sCountryISOCode=pays_code,
-#     _soapheaders=[header_value]
-# )
-# print(f"Flag  for {pays_code} is {result}")
-  
-# # make the service call
-# result = client.service.CountryIntPhoneCode(
-#     sCountryISOCode=pays_code,
-#     _soapheaders=[header_value]
-# )
-# print(f"Phone Code for {pays_code} is {result}")
-  
-
-# result = client.service.CapitalCity(
-#   sCountryISOCode=pays_code,
-#     _soapheaders=[header_value]
-# )
-
-# print(f"Capital for {pays_code} is {result}")
   
   
 def Infos_pays(code):
@@ -75,25 +23,21 @@ def Infos_pays(code):
   header_value = header(Action=method_url, To=service_url)
   client = zeep.Client(wsdl=wsdl_url)
   
-  result1 = client.service.CountryFlag(
+  drapeau = client.service.CountryFlag(
     sCountryISOCode=code,
       _soapheaders=[header_value]
   )
   
-  result2 = client.service.CountryIntPhoneCode(
+  codePays = client.service.CountryIntPhoneCode(
       sCountryISOCode=code,
       _soapheaders=[header_value])
       
-  result3 = client.service.CapitalCity(
+  capital = client.service.CapitalCity(
     sCountryISOCode=code,
     _soapheaders=[header_value]
   )
-  # json.dumps(key1=result1 ,
-  # key2=result2,
-  # key3=result3)
-  infos =[{'flag' :result1,'phone' : result2, 'capital' : result3}]
+
+  infos =[{'flag' :drapeau,'phone' : codePays, 'capital' : capital}]
   
   return infos
 
-
-#Infos_pays('BZ')
